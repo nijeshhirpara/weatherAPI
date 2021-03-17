072086bbf5244bff97936b30e3f97e4e19910465
Nijesh Hirpara <nijesh.hirpara@intersectalliance.com>
2021-03-17 22:14:28 +1030

    Added Secondary OpenWeatherMap API

--- webserver/api/weather.go
+++ webserver/api/weather.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
 
@@ -31,6 +32,12 @@ func HandleWeather(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if data, err := fetchFromWeatherStack(city[0]); err == nil {
+		content.Data = data
+		SendJsonResponse(w, content)
+		return
+	}
+
+	if data, err := fetchFromOpenWeatherMap(city[0]); err == nil {
 		content.Data = data
 		SendJsonResponse(w, content)
 		return
@@ -69,7 +76,50 @@ func fetchFromWeatherStack(city string) (weatherData, error) {
 	}
 
 	current := objx.New(result["current"])
-	wd.Temperature = current.Get("temperature").Float64()
-	wd.WindSpeed = current.Get("wind_speed").Float64()
+
+	// Response temperature is in celsius
+	wd.Temperature = math.Round((current.Get("temperature").Float64())*100) / 100
+
+	// Response Wind Speed is in km/hr
+	wd.WindSpeed = math.Round((current.Get("wind_speed").Float64())*100) / 100
+	return wd, nil
+}
+
+func fetchFromOpenWeatherMap(city string) (weatherData, error) {
+	var wd weatherData
+
+	accessKey := os.Getenv("OpenWeatherMap_AppID")
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?appid=%s&q=%s", accessKey, city)
+	res, err := http.Get(url)
+	if err != nil {
+		return wd, err
+	}
+
+	resData, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return wd, err
+	}
+
+	res.Body.Close()
+
+	// Declared an empty interface
+	var result map[string]interface{}
+
+	// Unmarshal or Decode the JSON to the interface.
+	json.Unmarshal([]byte(resData), &result)
+
+	obj := objx.New(result)
+	if status := obj.Get("cod").String(); status != "200" {
+		return wd, fmt.Errorf("Error fetching from weather stack API")
+	}
+
+	// Response temperature is in Kelvin, hence we need to convert it into Celsius
+	main := objx.New(result["main"])
+	wd.Temperature = math.Round((main.Get("temp").Float64()-273.15)*100) / 100 // Kelvin to Celsius
+
+	// Response wind speed is in mt/sec, hence we need to convert it into km/hr
+	wind := objx.New(result["wind"])
+	wd.WindSpeed = math.Round((wind.Get("speed").Float64()*3.6)*100) / 100 // mt/sec to km/hr
+
 	return wd, nil
 }
